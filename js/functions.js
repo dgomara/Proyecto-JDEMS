a816915e4a9371891a918ad7499326f2c23a8a8f
dgomara <cb500s_roja@hotmail.com>
2014-01-27 16:10:40 +0100

    Primer commit
    Commit Diego

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+﻿function cargar(){
+	var urlWeb = document.getElementById("url").value;
+	var ide = document.getElementById("identificador").value;
+	var url="php/extraer.php"
+	var parametros = {
+		"url" : urlWeb,
+		"ide" : ide
+	};
+	$.ajax({   
+		data:  parametros,
+		type: "POST",
+		url:url,
+		data:  parametros,
+		success: function(datos){       
+			$('#contenido').html(datos);
+		}
+	});
+}
---
+++ php/extraer.php
@@ -0,0 +1,7 @@
+﻿<?php
+	include_once('simple_html_dom.php');
+	$direccion = $_POST["url"];
+	$ide = $_POST["ide"];
+	$html = file_get_html('http://'.$direccion);
+	echo $html->getElementById($ide);
+?>
